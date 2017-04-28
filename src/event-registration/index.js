8dfdcfa18365273ec0fc8e393ab7f3dfff710b62
John-Olav Storvold <johsto@knowit.no>
2017-04-28 11:28:22 +0200

    Initial commit

---
+++ src/event-registration/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './component.css';
+
+class EventRegistration extends Component {
+  render() {
+    return (
+      <div className="event-registration">
+      </div>
+    );
+  }
+}
+
+export default EventRegistration;
---
+++ src/registration-row/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './component.css';
+
+class RegistrationRow extends Component {
+  render() {
+    return (
+      <div className="registration-row">
+        <p>Implement stuff here</p>
+      </div>
+    );
+  }
+}
+
+export default RegistrationRow;
