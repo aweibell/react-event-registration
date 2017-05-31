73f1b53a1ecae06c634824ced48928d6f20649a7
Atle Weibell <awe@knowit.no>
2017-05-31 13:40:59 +0200

    Added TODO

--- src/test/base.js
+++ src/test/base.js
@@ -2,6 +2,7 @@
 
 import Rebase from 're-base';
 
+// TODO: Move apiKey to environment variable using process.env
 const base = Rebase.createClass({
   apiKey: "AIzaSyBwBT4-fDAzPAJjvbbVPv4OaqqFu-RHdRE",
   authDomain: "joinin-react.firebaseapp.com",
