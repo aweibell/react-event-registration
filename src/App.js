ed7cc6fb2b19667431b00c1e7643617bac67cbd2
Atle Weibell <awe@knowit.no>
2017-06-25 14:55:08 +0200

    WIP

--- src/App.js
+++ src/App.js
@@ -1,20 +1,47 @@
 import React, { Component } from 'react'
-//import EventRegistration from "./components/EventRegistration";
-import ShowRegistrations from "./components/ShowRegistrations";
 import logo from './evreg_logo.svg'
 import './App.css'
 import base from './test/base';
+import auth from './test/base';
 import config from './test/data';
+import {
+  BrowserRouter,
+  Route,
+  Link
+} from 'react-router-dom';
+
+import Match from 'react-router';
+
+import Register from 'Register';
+
+export const storageKey = 'KEY_FOR_LOCAL_STORAGE';
+
+export const isAuthenticated = () => {
+  return !!auth.currentUser || !!localStorage.getItem(storageKey);
+};
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      registrations: {}
+      uid: null
     };
     this.onSubmitHandler = this.onSubmitHandler.bind(this);
     this.onChangeHandler = this.onChangeHandler.bind(this);
   }
+
+  componentDidMount() {
+    auth.onAuthStateChanged(user => {
+      if (user) {
+        window.localStorage.setItem(storageKey, user.uid);
+        this.setState({uid: user.uid});
+      } else {
+        window.localStorage.removeItem(storageKey);
+        this.setState({uid: null});
+      }
+    });
+  }
+
   componentWillMount() {
     this.ref = base.syncState(`/joinin-17-05/registrations`,
       {
@@ -40,23 +67,26 @@ class App extends Component {
   render () {
     const {formGroups, style } = config;
     return (
-      <div className='App'>
-        <div className='App-header'>
-          <img src={logo} className='App-logo' alt='logo' />
-        </div>
-        <h2 className="App-intro">Please register for our premier event!</h2>
-        <div className='component-test'>
-{/*          <EventRegistration formGroups={formGroups} style={style}
-                             data={this.state.registrations}
-                             // onSubmit={this.onSubmitHandler}
-                             onChange={this.onChangeHandler}/>*/}
-          <ShowRegistrations formGroups={formGroups}
-                             style={style}
-                             data={this.state.registrations}/>
-        </div>
-      </div>
+      <BrowserRouter>
+        <Match exactly pattern="/" component={JoinIn} />
+        <Match pattern="/login" component={Login} />
+        <MatchWhenAuthorized pattern="/protected" component={Register} />
+      </BrowserRouter>
     )
   }
 }
+
+const MatchWhenAuthorized = ({component: Component, ...rest}) => (
+  <Match {...rest} render={renderProps => (
+    isAuthenticated() ? (
+      <Component {...renderProps} />
+    ) : (
+      <Redirect to={ {
+        pathname: '/login',
+        state: {from: renderProps.location}
+      } } />
+    )
+  )}/>
+)
 
 export default App
--- src/test/base.js
+++ src/test/base.js
@@ -1,13 +1,17 @@
-
-
+import firebase from 'firebase';
 import Rebase from 're-base';
 
 // TODO: Move apiKey to environment variable using process.env
-const base = Rebase.createClass({
+const config = {
   apiKey: "AIzaSyBwBT4-fDAzPAJjvbbVPv4OaqqFu-RHdRE",
   authDomain: "joinin-react.firebaseapp.com",
   databaseURL: "https://joinin-react.firebaseio.com",
-});
+};
+//the root app just in case we need it
+const firebaseApp = firebase.initializeApp(config);
+
+const base = Rebase.createClass(config);
+
+export const auth = firebaseApp.auth(); //the firebase auth namespace
 
 export default base;
-
