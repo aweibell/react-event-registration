6b4bede22b807c3a1b5e31274095babb50b59d6b
Atle Weibell <awe@knowit.no>
2017-08-07 14:48:18 +0200

    Fixed bug in path to component

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import RegistrationCollection from './RegistrationCollection';
+import RegistrationCollection from './components/RegistrationCollection';
 
 class EventRegistration extends Component {
 
